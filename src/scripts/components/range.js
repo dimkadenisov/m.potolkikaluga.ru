e4871172102706a4522d4dcabecbb8ec7032cd58
Dima Denisov <2denisov@bk.ru>
2020-07-14 01:53:50 +0300

    another commit

--- src/scripts/components/range.js
+++ src/scripts/components/range.js
@@ -1,16 +1,11 @@
 const setRangeBackground = target => {
-	const min = target.min;
-	const max = target.max;
+	const min = target.min || 0;
+	const max = target.max || 100;
 
-	const percent = (max - min) * 0.01;
+	const percent = (max - min) / 100;
 	const value = (target.value - min) / percent;
 
-	$(target).css(
-		'background',
-		`linear-gradient(to right, #e50469 ${value}%, #e5e8ed ${value}%)`,
-	);
-
-	console.log(target.value, value);
+	target.style.background = `linear-gradient(to right, #e50469 ${value}%, #e5e8ed ${value}%)`;
 };
 
 document.body.addEventListener('input', e => {
